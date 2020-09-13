b409cc61d271c289e31246ef4dd3ae4104aff8fd
Satwaj Dhavale <thesatwaj@gmail.com>
2020-09-13 18:40:04 +0530

    Add files via upload

---
+++ Hadoop Assignment 3/Map.java
@@ -0,0 +1,27 @@
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+public class Map extends Mapper<LongWritable, Text, Text, IntWritable> {
+    private Text word = new Text();
+
+    public void map(LongWritable key, Text value, Context context)
+            throws IOException, InterruptedException {
+        String line = value.toString();
+        
+        String st [] = value.toString().split("\\s+");        
+        for(String st1 :  st) {
+   
+        	if(st1.startsWith("a")) {
+        		word.set("A_count");
+        		
+        		context.write(word, new IntWritable(st1.length())); 
+        	}
+        }
+
+    }
+}
---
+++ Hadoop Assignment 3/Reduce.java
@@ -0,0 +1,20 @@
+import java.io.IOException;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+public class Reduce extends Reducer<Text, IntWritable, Text, IntWritable> {
+
+    public void reduce(Text key, Iterable<IntWritable> values, Context context)
+            throws IOException, InterruptedException {
+        int sum = 0;
+        int count = 0;
+        for (IntWritable val : values) {
+            sum += val.get();
+            count += 1;
+        }
+        sum =  sum / count;
+        context.write(key, new IntWritable(sum));
+    }
+}
